c541d755a76d48afb4f02d41f5fd12b5db129fbf
Jim Bothén <jim.bothen@gmail.com>
2025-08-26 13:03:44 +0200

    Add first test.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,4 +5,12 @@ describe("todo", () => {
     cy.get("li").first().should("contain.text", "Feed the cat");
     cy.get("li").last().contains("Walk all the cats");
   });
+  it("should be able to delete todo", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("button").first().click();
+    cy.get("li")
+      .should("have.length", 2)
+      .first()
+      .should("not.contain.text", "Feed the cat");
+  });
 });
